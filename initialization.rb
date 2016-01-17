c74e3c261a711dbff8c144183940c628710ed2f4
Michael Sive <mikesive@gmail.com>
2016-01-17 16:54:26 +1000

    change load order

--- initialization.rb
+++ initialization.rb
@@ -4,7 +4,8 @@ require 'json'
 require 'byebug'
 
 puts 'Initializing...'.blue
-Dir.glob('./resources/**/*.rb', &method(:require))
+Dir.glob('./resources/*.rb', &method(:require))
+Dir.glob('./resources/*/*.rb', &method(:require))
 exchanges_config = Exchange.all
 exchanges_config.each do |config|
   puts "Configuring #{config['name']}...".blue
