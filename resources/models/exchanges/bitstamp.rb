bdc2f33565b00cba4aa60ed5eb25885f386e1c34
Michael Sive <mikesive@gmail.com>
2016-01-17 18:03:21 +1000

    refresh pairs

--- resources/models/exchanges/bitstamp.rb
+++ resources/models/exchanges/bitstamp.rb
@@ -2,6 +2,7 @@ class Bitstamp < Exchange
   private
 
   def data_formatter
+    @pairs = []
     @pair = Pair.new('BTC', 'USD',
                      @raw_data['ask'].to_f,
                      @raw_data['bid'].to_f,
