14575d7964ae9da7088011a72fe61cdf6e74175e
Michael Sive <mikesive@gmail.com>
2016-01-17 20:21:19 +1000

    take discreps out of loop

--- app.rb
+++ app.rb
@@ -1,17 +1,18 @@
 require './initialization.rb'
 @exchanges.each do |_name, exchange|
   exchange.refresh_data
-  possible_trades = Discrepencies.calculate(@exchanges)
-  possible_trades.each do |trade|
-    timestamp = DateTime.now.strftime('%Y-%m-%d %H:%M:%S')
-    base = trade[:base]
-    quote = trade[:quote]
-    buy = trade[:buy]
-    sell = trade[:sell]
-    diff = trade[:diff]
-    query = "insert into opportunities
-            (created_at, base, quote, buy_at, sell_at, diff) values
-            ('#{timestamp.to_s}', '#{base}', '#{quote}', '#{buy}', '#{sell}', #{diff})"
-    Utils.db.execute(query)
-  end
+end
+
+possible_trades = Discrepencies.calculate(@exchanges)
+possible_trades.each do |trade|
+  timestamp = DateTime.now.strftime('%Y-%m-%d %H:%M:%S')
+  base = trade[:base]
+  quote = trade[:quote]
+  buy = trade[:buy]
+  sell = trade[:sell]
+  diff = trade[:diff]
+  query = "insert into opportunities
+          (created_at, base, quote, buy_at, sell_at, diff) values
+          ('#{timestamp.to_s}', '#{base}', '#{quote}', '#{buy}', '#{sell}', #{diff})"
+  Utils.db.execute(query)
 end
