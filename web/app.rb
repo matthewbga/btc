df695d65bd81dd9e7abf4c07c055b9fd91cb6547
Michael Sive <mikesive@gmail.com>
2016-01-20 20:45:45 +1000

    limit 50

--- web/app.rb
+++ web/app.rb
@@ -30,6 +30,6 @@ class App < Sinatra::Base
 end
 
 get '/opportunities' do
-  @opportunities = Opportunity.all.order('created_at DESC')
+  @opportunities = Opportunity.all.order('created_at DESC').limit(50)
   slim :opportunities
 end
